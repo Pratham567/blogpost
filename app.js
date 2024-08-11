e7d294bb6dc25f5ca94f21e791e70b8ed303c142
Pratham Gupta <prathagu@cisco.com>
2024-08-11 23:29:54 +0530

    setup for supporting form input

--- app.js
+++ app.js
@@ -8,19 +8,13 @@ const app = express();
 const blogs = [
     { id: 1, title: 'Blog Title 1', summary: 'Summary of blog 1', content: 'Content of blog 1', author: 'Author 1', time: 'Time 1' },
     { id: 2, title: 'Blog Title 2', summary: 'Summary of blog 2', content: 'Content of blog 2', author: 'Author 2', time: 'Time 2' },
-    { id: 3, title: 'Blog Title 3', summary: 'Summary of blog 3', content: 'Content of blog 3', author: 'Author 3', time: 'Time 3' },
-    { id: 4, title: 'Blog Title 4', summary: 'Summary of blog 4', content: 'Content of blog 4', author: 'Author 4', time: 'Time 4' },
-    { id: 5, title: 'Blog Title 5', summary: 'Summary of blog 5', content: 'Content of blog 5', author: 'Author 5', time: 'Time 5' }
+    { id: 3, title: 'Blog Title 3', summary: 'Summary of blog 3', content: 'Content of blog 3', author: 'Author 3', time: 'Time 3' }
 ];
 
 const port = 3040;
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
-
-// Q: url encoding
-// Middleware to parse form data
-// app.use(bodyParser.urlencoded({ extended: true }));
 
 // register view engine
 app.set('view engine', 'ejs');
@@ -51,8 +45,13 @@ app.get('/newblog', (req, res) => {
     res.render('newblog', { title: 'New blog' });
 });
 
+// write a function to get the blog by id
+function getBlogById(id) {
+    return blogs.find(blog => blog.id === id);
+}
+
 // New route to view a single blog post
-app.get('/blogs/:id', (req, res) => {
+app.get('/blogs/id/:id', (req, res) => {
     const blogId = parseInt(req.params.id, 10);
     const blog = getBlogById(blogId);
     if (blog) {
@@ -62,44 +61,68 @@ app.get('/blogs/:id', (req, res) => {
     }
 });
 
-// 404 page
-app.use((req, res) => {
-    res.status(404).render('error', { title: 'Error' });
-});
-
-
-
-
-
-
-
-
-
-
-
-// write a function to get the blog by id
-function getBlogById(id) {
-    return blogs.find(blog => blog.id === id);
-}
-
 // write a function to add a new blog
 function addBlog(blog) {
     blogs.push(blog);
 }
 
+// Middleware to parse form data
+app.use(bodyParser.json());
+
+// POST request to add a new blog
+app.post('/blogs', (req, res) => {
+    const blog = req.body;
+    blog.id = blogs.length + 1;
+    blog.time = new Date().toLocaleString();
+    console.log(`New blog added: ${blog.title}`);
+    addBlog(blog);
+    res.redirect('/success');
+});
+
+// DELETE request to delete a blog
+app.delete('/blogs/id/:id', (req, res) => {
+    const blogId = parseInt(req.params.id);
+    deleteBlog(blogId);
+    res.redirect('/blogs');
+}
+);
+
 // write a function to delete a blog by id
 function deleteBlog(id) {
-    blogs = blogs.filter(blog => blog.id !== id);
+    const index = blogs.findIndex(blog => blog.id === id);
+    if (index !== -1) {
+        blogs.splice(index, 1);
+    }
 }
 
-
-
-
 app.get('/success', (req, res) => {
-    res.render('success', { title: 'Contact' });
+    res.render('success', { title: 'Success' });
 }
 );
 
 app.get('/fail', (req, res) => {
-    res.render('fail', { title: 'Contact' });
+    res.render('fail', { title: 'Failed' });
+});
+
+
+
+
+// 1. Let's submit a form
+// Q: What is the default method of the form?
+// Q: What is the default path of the form?
+// Q: What is the content type of the form? 
+// Q: What is url encoding?
+// Do we need to convert this url encoded data to JSON?
+// How to do that?
+// -> Use body-parser middleware
+// app.use(bodyParser.urlencoded({ extended: true }));
+// What is the difference between body-parser.urlencoded and body-parser.json?
+
+
+
+
+
+// 404 page
+app.use((req, res) => {
+    res.status(404).render('error', { title: 'Error' });
 });
