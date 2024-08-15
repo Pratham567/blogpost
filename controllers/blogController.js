4649a3d6f75230810207101730f15c1a1d6c5774
Pratham Gupta <prathagu@cisco.com>
2024-08-15 07:24:19 +0530

    Extracted blogControllers

--- controllers/blogController.js
+++ controllers/blogController.js
@@ -4,15 +4,80 @@
 // 3. Import these functions in the routes file
 
 
+const BlogPost = require('../models/blogpost');
 
 
 // blog_index_get
-// const blog_index_get = (req, res) => {
+const blog_index_get = (req, res) => {
+    BlogPost.find().sort({ createdAt: -1 })
+        .then((result) => {
+            res.render('blogs/blogs', { title: 'All blogs', blogs: result });
+        })
+        .catch((err) => {
+            console.log(err);
+            res.redirect('blogs/fail');
+        });
+}
 
 
+// blog_id_get
+const blog_id_get = (req, res) => {
+    BlogPost.findById(req.params.id)
+        .then((blog) => {
+            res.render('blogs/single-blog', { title: blog.title, blog });
+        })
+        .catch((err) => {
+            console.log(err);
+            res.status(404).render('error', { title: 'Blog Not Found' });
+        });
+}
+
+// blog_create_post
+const blog_create_post = (req, res) => {
+    const blog = req.body;
+    const blogpost = new BlogPost(blog);
+    blogpost.save()
+        .then((result) => {
+            console.log(`New blog added: ${result.title}`);
+            res.redirect('blogs/success');
+        })
+        .catch((err) => {
+            console.log(err);
+            res.redirect('/blogs/fail');
+        });
+
+    
+    // Create a new blog post using the BlogPost static method
+    // BlogPost.create(blog)
+    //     .then((result) => {
+    //             console.log(`New blog added: ${result.title}`);
+    //             res.redirect('/blogs/success');
+    //         })
+    //         .catch((err) => {
+    //             console.log(err);
+    //             res.redirect('/blogs/fail');
+    //         });
+    console.log(`New blog added: ${blog.title}`);
+}
 
 
+// blog_delete
+const blog_delete = (req, res) => {
+    BlogPost.findByIdAndDelete(req.params.id)
+        .then((result) => {
+            console.log(`Blog deleted: ${result.title}`);
+            res.json({ redirect: 'blogs/blogs' });
+        })
+        .catch((err) => {
+            console.log(err);
+            res.redirect('blogs/fail');
+        });
+}
 
-// modules.exports = {
-//     blog_index_get
-// }
+
+module.exports = {
+    blog_index_get,
+    blog_id_get,
+    blog_create_post,
+    blog_delete
+}
--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -1,94 +1,40 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const BlogPost = require('../models/blogpost');
+const blogController = require('../controllers/blogController');
 
 const router = express.Router();
 
 
 router.get('/form', (req, res) => {
-    res.render('blog/newblog', { title: 'New blog' });
+    res.render('blogs/newblog', { title: 'New blog' });
 });
 
 router.get('/success', (req, res) => {
-    res.render('blog/success', { title: 'Success' });
+    res.render('blogs/success', { title: 'Success' });
 }
 );
 
 router.get('/fail', (req, res) => {
-    res.render('blog/fail', { title: 'Failed' });
+    res.render('blogs/fail', { title: 'Failed' });
 });
 
-// blog routes
-router.get('/', (req, res) => {
-    BlogPost.find().sort({ createdAt: -1 })
-        .then((result) => {
-            res.render('blog/blogs', { title: 'All blogs', blogs: result });
-        })
-        .catch((err) => {
-            console.log(err);
-            res.redirect('blog/fail');
-        });
-});
-
-
-// New route to view a single blog post
-router.get('/id/:id', (req, res) => {
-    BlogPost.findById(req.params.id)
-        .then((blog) => {
-            res.render('blog/single-blog', { title: blog.title, blog });
-        })
-        .catch((err) => {
-            console.log(err);
-            res.status(404).render('error', { title: 'Blog Not Found' });
-        });
-});
-
-
+// // blog routes
 // Middleware to parse form data
 router.use(bodyParser.json());
-
 // POST request to add a new blog
 router.use(bodyParser.urlencoded({ extended: true }));
-router.post('/', (req, res) => {
-    const blog = req.body;
-    const blogpost = new BlogPost(blog);
-    blogpost.save()
-        .then((result) => {
-            console.log(`New blog added: ${result.title}`);
-            res.redirect('blog/success');
-        })
-        .catch((err) => {
-            console.log(err);
-            // TODO: Fixme
-            res.redirect('/blog/fail');
-        });
 
-    // Create a new blog post using the BlogPost static method
-    // BlogPost.create(blog)
-    //     .then((result) => {
-    //             console.log(`New blog added: ${result.title}`);
-    //             res.redirect('/success');
-    //         })
-    //         .catch((err) => {
-    //             console.log(err);
-    //             res.redirect('/fail');
-    //         });
-    console.log(`New blog added: ${blog.title}`);
-});
+// GET request to view all blogs
+router.get('/', blogController.blog_index_get);
 
+// New route to view a single blog post
+router.get('/id/:id', blogController.blog_id_get);
+
+// POST request to create a new blog
+router.post('/',blogController.blog_create_post);
 
 // DELETE request to delete a blog
-router.delete('/id/:id', (req, res) => {
-    BlogPost.findByIdAndDelete(req.params.id)
-        .then((result) => {
-            console.log(`Blog deleted: ${result.title}`);
-            res.json({ redirect: 'blog/blogs' });
-        })
-        .catch((err) => {
-            console.log(err);
-            res.redirect('blog/fail');
-        });
-});
+router.delete('/id/:id', blogController.blog_delete);
 
 module.exports = router;
-
