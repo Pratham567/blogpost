454a95b6ec3efeb3153b744f9078be9b7bb2c1fe
Pratham Gupta <prathagu@cisco.com>
2024-08-15 07:33:36 +0530

    install body-parser

--- routes/auth/authRoutes.js
+++ routes/auth/authRoutes.js
@@ -1,8 +1,12 @@
 
 const { Router } = require('express');
+const bodyParser = require('body-parser');
 const authController = require('../../controllers/authController');
 
 const router = Router();
+
+// Middleware to parse form data
+router.use(bodyParser.urlencoded({ extended: true }));
 
 router.get('/login', authController.login_get);
 
